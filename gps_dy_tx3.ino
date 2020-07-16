477b0b460a8f8295a9c99d3e16aef20c49884f92
ssk8 <ssk8@users.noreply.github.com>
2020-07-16 16:08:27 -0500

    added button

--- gps_dy_tx3.ino
+++ gps_dy_tx3.ino
@@ -2,10 +2,18 @@
 #include "nRF24L01.h"
 #include "RF24.h"
 #include <TinyGPS++.h>
+#include <EasyButton.h>
+
+
+#define LED_PIN 13
+#define BUTTON_1_PIN 10
+bool button1_state = false;
 
 
 RF24 radio(A4,A5);
 TinyGPSPlus gps;
+
+EasyButton button1(BUTTON_1_PIN);
 
 const uint64_t pipes[2] = { 0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL };
 
@@ -40,6 +48,14 @@ void setup(void)
   radio.openReadingPipe(1,pipes[1]);
 
   //radio.printDetails();
+
+  pinMode(LED_PIN, OUTPUT);
+
+  pinMode(BUTTON_1_PIN,INPUT_PULLUP);
+  button1.onPressed(buttonPressed);
+  button1.onSequence(2, 500, sequenceEllapsed);
+  button1.enableInterrupt(buttonISR);
+  button1.read();
 }
 
 void loop(void)
@@ -54,7 +70,7 @@ void loop(void)
     gps_data.speed = (short)gps.speed.mph();
     gps_data.course = (short)gps.course.value();
     gps_data.altitude = gps.altitude.value();
-    gps_data.button1 = false;
+    gps_data.button1 = button1_state;
     
 //    Serial.print(gps.time.value());
 //    Serial.print("  ");
@@ -63,4 +79,27 @@ void loop(void)
     radio.write( &gps_data, sizeof(GPS_data) ); 
   }
   
+}
+
+
+void buttonPressed()
+{
+  button1_state = !button1_state;
+  //Serial.print("rec=");
+  if (button1_state){
+    digitalWrite(LED_PIN, HIGH);
+  }
+  else{
+    digitalWrite(LED_PIN, LOW);
+    }
+}
+
+void sequenceEllapsed()
+{
+  Serial.println("Double click");
+}
+
+void buttonISR()
+{
+  button1.read();
 }
