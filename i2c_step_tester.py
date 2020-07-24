4a5069622303f00f651022cce8a3bbf9719fe483
ssk8 <ssk8@users.noreply.github.com>
2020-07-23 21:16:23 -0500

    Update i2c_step_tester.py

--- i2c_step_tester.py
+++ i2c_step_tester.py
@@ -1,21 +1,18 @@
+#!/usr/bin/python3
 
 import smbus
 
-bus = smbus.SMBus(1)    # 0 = /dev/i2c-0 (port I2C0), 1 = /dev/i2c-1 (port I2C1)
+bus = smbus.SMBus(1)
 
-DEVICE_ADDRESS = 0x08      #7 bit address (will be left shifted to add the read write bit)
-DEVICE_REG_MODE1 = 0x00
+DEVICE_ADDRESS = 0x08
 
-# n =65278
-# bus.write_block_data(DEVICE_ADDRESS, DEVICE_REG_MODE1, list(divmod(n, 255)))
-
-n = 65278
-bus.write_block_data(DEVICE_ADDRESS, DEVICE_REG_MODE1, list(divmod(n, 255)))
+bus.write_block_data(DEVICE_ADDRESS, 0x00, list(divmod(65278, 255)))
 
 try:
     while True:
-        n = int(input("what ps: "))
-        bus.write_block_data(DEVICE_ADDRESS, DEVICE_REG_MODE1, list(divmod(n, 255)))
+        n = int(input("what position? "))
+        bus.write_block_data(DEVICE_ADDRESS, 0x00, list(divmod(n, 255)))
 
 except KeyboardInterrupt:
-    bus.write_block_data(DEVICE_ADDRESS, DEVICE_REG_MODE1, list(divmod(65279, 255)))
+    bus.write_block_data(DEVICE_ADDRESS, 0x00, list(divmod(65279, 255)))
+    
