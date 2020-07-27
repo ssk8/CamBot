1c7445e23653141ad9558b503eb2924d89a6db77
ssk8 <ssk8@users.noreply.github.com>
2020-07-26 21:24:32 -0500

    Update display_img.py

--- display_img.py
+++ display_img.py
@@ -6,8 +6,10 @@ from PIL import Image
 import adafruit_rgb_display.st7789 as st7789
 from io import BytesIO
 from picamera import PiCamera
+from datetime import datetime
+from os import mkdir
 
-disp = st7789.ST7789(board.SPI(), rotation=90, width=240, height=240, x_offset=0, y_offset=80, cs=digitalio.DigitalInOut(board.CE0), dc=digitalio.DigitalInOut(board.D25), baudrate=24000000)
+miniTFT = st7789.ST7789(board.SPI(), rotation=90, width=240, height=240, x_offset=0, y_offset=80, cs=digitalio.DigitalInOut(board.CE0), dc=digitalio.DigitalInOut(board.D25), baudrate=24000000)
 
 backlight = digitalio.DigitalInOut(board.D22)
 backlight.switch_to_output()
@@ -18,18 +20,51 @@ buttonB = digitalio.DigitalInOut(board.D24)
 buttonA.switch_to_input()
 buttonB.switch_to_input()
 
-try:
 
-    while True:
-        if not buttonA.value:
-            print('well, helllo')
-            camera = PiCamera()
-            camera.resolution = (240, 240)
-            stream = BytesIO()
-            camera.capture(stream, format='jpeg')
-            stream.seek(0)
-            disp.image(Image.open(stream))
-            camera.close()
-except KeyboardInterrupt:
-    camera.close()
-    backlight.value = False
+def update_display_image(cam):
+    cam.resolution = (240, 240)
+    stream = BytesIO()
+    cam.capture(stream, format='jpeg')
+    miniTFT.image(Image.open(stream))
+    stream.close()
+
+
+def update_display_zoom_image(cam):
+    res = (4056, 3040) #(2028, 1520) 
+    cam.resolution = res
+    stream = BytesIO()
+    cam.capture(stream, format='jpeg')
+    miniTFT.image(Image.open(stream).crop((res[0]/2, res[1]/2, res[0]/2 + 240, res[1]/2 + 240)))
+    stream.close()
+
+
+def take_picture(cam):
+    cam.resolution = (4056, 3040)
+    try:
+        cam.capture(f'/home/pi/Pictures/{datetime.now().strftime("%y%m%d%H%M%S")}.jpg', format='jpeg')
+    except FileNotFoundError:
+        mkdir("/home/pi/Pictures")
+        take_picture(cam)
+
+
+def main():
+    zoom = False
+    try:
+        camera = PiCamera()
+        while True:
+            if not buttonA.value:
+                zoom = not zoom
+            if not buttonB.value:
+                take_picture(camera)
+
+            if not zoom:
+                update_display_image(camera)
+            else:
+                update_display_zoom_image(camera)
+    except KeyboardInterrupt:
+        camera.close()
+        backlight.value = False
+
+
+if __name__ == "__main__":
+    main()
