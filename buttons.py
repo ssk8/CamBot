40fe4706fa5709008177fe0ee6ddc0f4483729c9
ssk8 <stinkyskate@pm.me>
2021-07-01 08:57:54 -0500

    finish button mod

--- buttons.py
+++ buttons.py
@@ -3,20 +3,37 @@ from time import sleep
 
 
 class Buttons(object):
-    """handle buttons"""
+    """handle buttons A and B. When read on A or B return if a button has been pressed since last polled"""
 
-    def __init__(self, A_pin=23, B_pin=24):
+    def __init__(self, A_pin=24, B_pin=23):
         self._A_pin = A_pin
         self._B_pin = B_pin
-        self.button_A = False
-        self.button_B = False
+        self._A_state = False
+        self._B_state = False
         self.begin()
 
     def button_A_event(self, channel):
-        self.button_A = True
+        self._A_state = True
+    
+    @property
+    def A(self):
+        if self._A_state:
+            self._A_state = False
+            return True
+        else:
+            return False
 
     def button_B_event(self, channel):
-        self.button_B = True
+        self._B_state = True
+
+    @property
+    def B(self):
+        if self._B_state:
+            self._B_state = False
+            return True
+        else:
+            return False
+
 
     def begin(self):
         GPIO.setmode(GPIO.BCM)
@@ -26,12 +43,10 @@ class Buttons(object):
         GPIO.add_event_detect(self._B_pin, GPIO.FALLING, callback=self.button_B_event, bouncetime=200)
 
 def main():
-    buttons = Buttons()
+    button = Buttons()
     while True:
         sleep(1)
-        print(f"button A: {buttons.button_A}  button B: {buttons.button_B}")
-        if buttons.button_A:
-            buttons.button_A = False
+        print(f"button A: {button.A}  button B: {button.B}")
 
 
 if __name__ == "__main__":
